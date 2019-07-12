a36a8bd05b2deba10641fc5b8fb2743f947abc4d
wangyd2 <wangyd2@mapbar.com>
2019-07-12 19:31:38 +0800

    improve

--- vping.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-add_list = ["66.112.221.61","c19s1.jamjams.net","c19s2.jamjams.net","c19s3.jamjams.net","c19s4.jamjams.net","c19s5.jamjams.net"]
-processes = []
-
-#for address in add_list:
-#	processes.append(subprocess.Popen(["ping",address], stdout=subprocess.PIPE))
-for address in add_list:
-	subprocess.call(["ping",address])
