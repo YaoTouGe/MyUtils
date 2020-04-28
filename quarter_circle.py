2dcacf47075dc856d4feeb80ccaf7f0e8996accc
wangyd2 <wangyd2@mapbar.com>
2020-04-28 19:34:12 +0800

    update output format

--- quarter_circle.py
+++ quarter_circle.py
@@ -17,19 +17,19 @@ for quaterId in range(4):
     rad = quaterId * math.pi * 0.5
     x = round(radius * math.cos(rad))
     y = round(radius * math.sin(rad))
-    print "{", x, ",", -y, "},"
+    print "{ " +str(x) + "f, " + str(-y) + "f },"
     rad = rad + radianDelta
 
     for i in range(1, sampleNum - 1):
         x = radius * math.cos(rad)
         y = radius * math.sin(rad)
 
-        print "{", x, ",", -y, "},"
+        print "{ " +str(x) + "f, " + str(-y) + "f },"
         rad = rad + radianDelta
     
     rad = math.pi * 0.5 * (quaterId + 1)
     x = round(radius * math.cos(rad))
     y = round(radius * math.sin(rad))
     
-    print "{", x, ",", -y, "},"
+    print "{ " +str(x) + "f, " + str(-y) + "f },"
     rad = rad + radianDelta
