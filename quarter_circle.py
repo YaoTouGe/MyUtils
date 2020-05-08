66da5e5e2eae9216abd7744001abe70c7081669e
wangyd2 <wangyd2@mapbar.com>
2020-05-08 12:13:54 +0800

    update

--- quarter_circle.py
+++ quarter_circle.py
@@ -9,27 +9,23 @@ if len(sys.argv) == 2:
 elif len(sys.argv) == 3:
     sampleNum = int(sys.argv[2])
 
-radianDelta = math.pi * 0.5 / (sampleNum - 1)
+radianDelta = math.pi * 0.5 / sampleNum
 rad = 0
 
-for quaterId in range(4):
-    print "quater", quaterId
-    rad = quaterId * math.pi * 0.5
-    x = round(radius * math.cos(rad))
-    y = round(radius * math.sin(rad))
-    print "{ " +str(x) + "f, " + str(-y) + "f },"
-    rad = rad + radianDelta
+for quarterId in range(4):
+    rad = math.pi * 0.5 * quarterId
+    x = math.cos(rad)
+    y = math.sin(rad)
+    print "{" + str(round(x)) + "f," +str(round(y)) + "f},"
 
-    for i in range(1, sampleNum - 1):
-        x = radius * math.cos(rad)
-        y = radius * math.sin(rad)
-
-        print "{ " +str(x) + "f, " + str(-y) + "f },"
+    for i in range(sampleNum - 2):
         rad = rad + radianDelta
+        x = math.cos(rad)
+        y = math.sin(rad)
+        print "{" + str(x) + "f," +str(y) + "f},"
     
-    rad = math.pi * 0.5 * (quaterId + 1)
-    x = round(radius * math.cos(rad))
-    y = round(radius * math.sin(rad))
-    
-    print "{ " +str(x) + "f, " + str(-y) + "f },"
-    rad = rad + radianDelta
+    rad = math.pi * 0.5 * (quarterId + 1)
+    x = math.cos(rad)
+    y = math.sin(rad)
+    print "{" + str(round(x)) + "f," +str(round(y)) + "f},"
+    print
